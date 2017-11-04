1f6bfa90cd390967c9efb99f65845b4ddbe8fe4c
Jannis Konrad <kabel42@gmail.com>
2017-11-04 22:29:00 +0100

    fix negative numbers

--- ina219.py
+++ ina219.py
@@ -1,4 +1,5 @@
 from machine import Pin, I2C
+import ustruct
 
 class INA219(object):
     def __init__(self, i2c=None, scl=None, sda=None,Rsh=0.1,BRNG=1,PG=3,BADC=3,SADC=3,MODE=7, addr=64):
@@ -20,13 +21,17 @@ class INA219(object):
         calstr[2] = ((BADC&0x01)<<7)+((SADC&0x0F)<<3)+(MODE&0x07)
         self.i2c.writeto(self.addr, calstr)
         
-    def read(self):
+    def readI(self):
         self.i2c.writeto(self.addr, '\1') #select Reg1 Shunt Voltage
-        hi, lo = self.i2c.readfrom(self.addr, 2)
-        Ishunt = ((hi<<8)+lo)*self.Rsh #100uV/0.1Ω=1mA
-        
+        data = self.i2c.readfrom(self.addr, 2)
+        data = ustruct.unpack('!h', data)[0]
+        return data*self.Rsh #100uV/0.1Ω=1mA
+    
+    def readV(self):
         self.i2c.writeto(self.addr, '\2') #select Reg2 Bus Voltage
-        hi, lo = self.i2c.readfrom(self.addr, 2)
-        Vbus = (((hi<<8)+lo)>>3)*4.0 #mV
-        
-        return Ishunt, Vbus
+        data = self.i2c.readfrom(self.addr, 2)
+        data = ustruct.unpack('!h', data)[0]
+        return (data>>3)*4.0 #mV
+    
+    def read(self):
+      return self.readI(), self.readV()
